50d522357d2ae684e65ddd52a0c276d4fa4d1518
naderalfakesh <naderalfakesh@gmail.com>
2019-12-26 05:12:26 +0300

    program structure is better and understandable

--- main.js
+++ main.js
@@ -1,38 +1,31 @@
 import Controller from "./Controller.js";
 import Player from "./Player.js";
 
-
-const $player = $('#player')    
-const $frame = $('#frame')
-
-
-
-
-
-const enviroment= {
-    gravity: { x: 0 , y: 0.1},
-    friction: {
-         air: {x:0 ,y: 0.99 },
-         ground: {x:0 ,y: 0 }
-    }
-}
-
 function update(){
-    console.log(Player.jumping)
+
     Player.checkBoundries();
+
     Player.updatePosition();
+
     if (Controller.keyPressed && !Player.jumping){ 
         console.log("jump")
         Player.velocity.y = -3 ;
         Player.jumping = true;
     }
-    if(Player.jumping){Player.jump();Controller.keyPressed = false;}
-    window.requestAnimationFrame(update);
+
+    if(Player.jumping){
+        Player.jump();
+        Controller.keyPressed = false;
+    }
+
+    
+    window.requestAnimationFrame(update); // updating animation after calculation is done
     }
 
 
  
 
-
+// listening for jump key press
 window.addEventListener("keypress" ,Controller.keyListner )
+// requesting animation frame
 window.requestAnimationFrame(update);
